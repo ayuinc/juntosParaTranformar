951d98ecafeadee6e66cf25e86388827482c870b
katycoder <dayis_sc@hotmail.com>
2016-05-31 19:46:47 -0500

    formulario

--- app/assets/js/validate.js
+++ app/assets/js/validate.js
@@ -20,3 +20,26 @@ $(document).ready(function() {
       return validar(e);
   });
 });
+
+$(document).ready(function() {
+    $('#freeform_categoria_de_postulacion_1').attr( "checked","checked" );
+    $("input[name$='categoria_de_postulacion']").click(function() {
+        // var test = $(this).();
+        if ($(this).val() != 'Si') {
+
+          $("form>div.ff_composer>div:nth-child(5)").children().eq(10).children().eq(1).addClass('visibility');
+        }
+        else {
+          $(".ff_composer").children().eq(10).children().eq(1).removeClass('hidden');
+        }
+
+    });
+
+    $('#freeform_esta_inscrita_en_registros_publicos_2').click(function() {
+        if ($(this).val() == 'No') {
+
+          $("input[name='ruc']").val("");
+          $("input[name='cuando_se_constituyeron_dd_mm_yyyy']").val("");
+        }
+    });
+});
