426845decfd93ffd890fa2bf8595052eb88c8e82
katycoder <dayis_sc@hotmail.com>
2016-06-06 16:08:31 -0500

    becas-formulario

--- app/assets/js/validate.js
+++ app/assets/js/validate.js
@@ -27,6 +27,12 @@ $(document).ready(function() {
     $("#freeform_categoria_de_postulacion_3").click(function(){
       $("form>div.ff_composer>div:nth-child(5)").removeClass("hidden");
     });
+    $("#freeform_categoria_de_postulacion_2").click(function(){
+      $("form>div.ff_composer>div:nth-child(5)").addClass("hidden");
+    });
+    $("freeform_categoria_de_postulacion_1").click(function(){
+      $("form>div.ff_composer>div:nth-child(5)").adClass("hidden");
+    });
 });
 
 $(document).ready(function() {
