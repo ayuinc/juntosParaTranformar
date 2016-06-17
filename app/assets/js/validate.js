265c872ed08b6bde184de2207d3e7abec98e750a
katycoder <dayis_sc@hotmail.com>
2016-06-17 16:24:06 -0500

    postula

--- app/assets/js/validate.js
+++ app/assets/js/validate.js
@@ -44,4 +44,8 @@ $(document).ready(function() {
 
 $('#freeform_fecha_de_nacimiento,#freeform_fecha_de_inicio_dd-mm-yyyy,#freeform_fecha_de_termino_dd-mm-yyyy,#freeform_fecha_de_inicio,#freeform_fecha_de_termino_a_la_actualidad,#freeform_fecha_de_inicio_exp,#freeform_fecha_de_termino_a_la_actualidad_exp').attr('type',"date");
 
-
+$('#freeform_logros').attr('maxlength', '100').parent().addClass('textarea-wrapper');
+$('#freeform_logros_exp').attr('maxlength', '100').parent().addClass('textarea-wrapper');                 
+$('#freeform_que_otra_beca_pasantia_has_ganado_y_en_que_ano').attr('maxlength', '100').parent().addClass('textarea-wrapper');
+$('#freeform_en_que_pais_te_gustaria_realizar_tu_pasantia_puedes_nombrar_3_paises_por_orden_de_preferencia').attr('maxlength', '100').parent().addClass('textarea-wrapper');
+$('#freeform_en_que_restaurante_establecimiento_te_gustaria_realizar_tu_pasantia_puedes_nombrar_3_lugares_por_orden_de_preferencia').attr('maxlength', '100').parent().addClass('textarea-wrapper');
